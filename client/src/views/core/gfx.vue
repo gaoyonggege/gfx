48b01582ec9a252408b6f69b9d209bbc5895da9a
gaoyong <gaoyong@weiyankeji.cn>
2018-03-31 14:54:11 +0800

    gfx页面样式小调整

--- client/src/views/core/gfx.vue
+++ client/src/views/core/gfx.vue
@@ -5,7 +5,14 @@
 <template>
     <section class="page gfx">
         <p class="title">GFX系统文档</p>
-        
+        <ul class="card">
+            <li>
+                <p>部署机器IP: <span>{{ serverIp }}</span></p>
+            </li>
+            <li>
+                <p>测试环境静态资源的的域名路径前缀: <span>{{ serverPrefixPath }}</span></p>
+            </li>
+        </ul>
     </section>
 </template>
 
@@ -14,7 +21,11 @@
 export default {
     data () {
         return {
-
+            // 部署的机器ip
+            serverIp: '..',
+            // 测试环境的域名路径前缀
+            serverPrefixPath: '..',
+            
         };
     }
 }    
@@ -22,11 +33,29 @@ export default {
 
 <style lang="scss" scoped>
     .gfx {
+        padding: 40px 8%;
+
         .title {
+            margin-bottom: 30px;
             text-align: center;
+            font-weight: bold;
             font-size: 24px;
         }
 
-        
+        .card {
+            margin: 30px 0;
+            padding: 15px;
+            background: rgba(3, 15, 9, .1);
+            
+
+            p {
+                line-height: 2;
+                font-size: 18px;
+
+                span {
+                    color: rgba(219, 26, 12, 0.5);
+                }
+            }
+        }
     }    
 </style>
